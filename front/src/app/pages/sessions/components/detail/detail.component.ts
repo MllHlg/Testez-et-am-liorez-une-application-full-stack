af9275c6b94756751f8b653279bd5998d90d3994
MllHlg <maelle772001@gmail.com>
2026-03-30 14:38:53 +0200

    Refactor: suppression des any et typage des méthodes.

--- front/src/app/pages/sessions/components/detail/detail.component.ts
+++ front/src/app/pages/sessions/components/detail/detail.component.ts
@@ -45,7 +45,7 @@ export class DetailComponent implements OnInit {
     this.fetchSession();
   }
 
-  public back() {
+  public back(): void {
     window.history.back();
   }
 
@@ -53,7 +53,7 @@ export class DetailComponent implements OnInit {
     this.sessionApiService
       .delete(this.sessionId)
       .pipe(takeUntilDestroyed(this.destroyRef))
-      .subscribe((_: any) => {
+      .subscribe(() => {
         this.matSnackBar.open('Session deleted !', 'Close', { duration: 3000 });
         this.router.navigate(['sessions']);
       }
