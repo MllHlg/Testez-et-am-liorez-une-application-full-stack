a34bea45743c2d68183cb2843df83dba40c42764
MllHlg <maelle772001@gmail.com>
2026-03-30 12:30:33 +0200

    Refactor: remplacement des *ngIf et des *ngFor.

--- back/src/main/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwt.java
+++ back/src/main/java/com/openclassrooms/starterjwt/security/jwt/AuthEntryPointJwt.java
@@ -1,17 +1,7 @@
 package com.openclassrooms.starterjwt.security.jwt;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Component
